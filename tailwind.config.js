ace9463c66cde55a47bb7f2afe3f344411ab2463
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-10-17 12:53:20 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/assets/js/*.{js}", "./**/*.{html}"],
+  content: ["./index.html", "./**/*.{html}"],
   theme: {
     extend: {},
   },
