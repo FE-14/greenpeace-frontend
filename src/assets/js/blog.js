25b6c07c16cb2af2a31f062e2af3d4203e734762
ersankarimi <ersankarimii311@gmail.com>
2022-10-22 03:46:21 +0800

    Update blog.js

--- src/assets/js/blog.js
+++ src/assets/js/blog.js
@@ -1,4 +1,9 @@
-import { useState, getArticlePostData } from "./modules/index.js";
+import {
+  useState,
+  getArticlePostData,
+  createTagElements,
+  formatPublishedDate,
+} from "./modules/index.js";
 
 const API_URL = "https://634c1520317dc96a3090f23c.mockapi.io/greenpeace/api/v1";
 const id = window.location.search.split("=")[1];
@@ -16,23 +21,6 @@ let [articlePostDataPromise] = useState(
   getArticlePostData(`${API_URL}/posts/${id}`)
 );
 
-const formatPublishedDate = (postDate) => {
-  const publishedDate = new Date(postDate).toLocaleDateString("id-Id", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-  return publishedDate;
-};
-
-const tagElements = (tags) => {
-  return tags
-    .map((tag) => {
-      return `<span class="text-md p-4 bg-green-gp-100 text-submarine-gp-500">${tag}</span>`;
-    })
-    .join("");
-};
-
 articlePostDataPromise.then((data) => {
   console.log(data);
   blogTitleElement.textContent = data.title;
@@ -40,7 +28,7 @@ articlePostDataPromise.then((data) => {
   blogWriterElement.textContent = data.authorName;
   blogPostdateElement.textContent = formatPublishedDate(data.postDate);
   blogDescElement.textContent = data.postDescription;
-  blogTagsElement.innerHTML = tagElements(data.tags);
+  blogTagsElement.innerHTML = createTagElements(data.tags);
   blogImgElement.src = data.imageUrl;
   blogContentElement.innerHTML = data.postContent;
 });
