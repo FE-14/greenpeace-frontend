2c27f0ac8e203fb838e0725cde3c0668e9d4f220
ersankarimi <ersankarimii311@gmail.com>
2022-10-24 20:32:24 +0800

    feat: add the screens value

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/assets/js/**/*.js", "./**/*.html"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         white: "#ffffff",
