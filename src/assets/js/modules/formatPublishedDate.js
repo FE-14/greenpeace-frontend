9b4be369f06371f0ebd621760b08daa8a6fd9f26
ersankarimi <ersankarimii311@gmail.com>
2022-11-13 13:59:39 +0800

    refactor: setup the `react` project boilerplate
    Setup the react project boilerplate and install some additionals libraries , such as react router, tailwind css, prop types, redux, vitest, eslint, prettier, and framer motion.

--- src/assets/js/modules/formatPublishedDate.js
+++
@@ -1,8 +0,0 @@
-export const formatPublishedDate = (postDate) => {
-  const publishedDate = new Date(postDate).toLocaleDateString("id-Id", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-  return publishedDate;
-};
