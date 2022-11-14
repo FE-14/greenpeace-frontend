5d4ea8bc855543f3258dfad8e0b70fa162d2df33
ersankarimi <ersankarimii311@gmail.com>
2022-11-14 13:20:59 +0800

    feat: create the index.js
    The index.js files will import and re-export the modules from that directories.

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import articleHero from "./images/article-hero.webp";
+import greenBigOutlineHalfOutboard from "./images/green-big-outline-half-artboard.png";
+import greenFullArtboard from "./images/green-full-artboard.png";
+import greenLineIllustration from "./images/green-line-illustration.png";
+import greenOutlineHalfArtboard from "./images/green-outline-half-artboard.png";
+import greenpeaceLogoWhite from "./images/greenpeace-logo-white.png";
+import homepageHero from "./images/homepage-hero.webp";
+import whiteLineIllustration from "./images/white-line-illustration.png";
+
+export {
+  articleHero,
+  greenBigOutlineHalfOutboard,
+  greenFullArtboard,
+  greenLineIllustration,
+  greenOutlineHalfArtboard,
+  greenpeaceLogoWhite,
+  homepageHero,
+  whiteLineIllustration,
+};
