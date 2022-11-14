f1c5c18283185183aacabae13724b7b8829c6476
ersankarimi <ersankarimii311@gmail.com>
2022-11-14 20:51:00 +0800

    refactor: handle the responsive of the Navbar component
    Update some classes' style names for some elements that will support the responsiveness of the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   HiOutlineMenuAlt3,
   HiOutlineMinus,
@@ -11,12 +11,17 @@ import { AnimatePresence, motion } from "framer-motion";
 import { greenpeaceLogoWhite } from "../assets";
 
 export default function Navbar() {
-  const [navbarIsOpen, setNavbarIsOpen] = useState(false);
+  const [isUnderLargeSizeWidth, setIsUnderLargeSizeWidth] = useState(
+    window.innerWidth < 768
+  );
+
+  const [navbarIsOpen, setNavbarIsOpen] = useState(!isUnderLargeSizeWidth);
+
   const ulVariants = {
     hidden: {
       opacity: 0,
       scale: 0,
-      y: -1000,
+      y: isUnderLargeSizeWidth ? -1000 : 0,
     },
     visible: {
       opacity: 1,
@@ -32,15 +37,28 @@ export default function Navbar() {
       y: -1000,
     },
   };
+
+  const handleResize = () => {
+    setIsUnderLargeSizeWidth(window.innerWidth < 768);
+    setNavbarIsOpen(!isUnderLargeSizeWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerWidth]);
+
   return (
-    <header className="basic-padding fixed w-full bg-green-gp-800/[55%] py-4 font-montserrat backdrop-blur-md">
+    <header className="basic-padding fixed z-50 w-full  bg-green-gp-800/[35%] py-4 font-montserrat backdrop-blur-sm">
       <nav className="flex items-center justify-between">
         <figure>
           <Link to="/">
             <img
               src={greenpeaceLogoWhite}
               alt="Greenpeace Logo"
-              className="h-6"
+              className="h-6 lg:h-8"
             />
           </Link>
         </figure>
@@ -49,31 +67,35 @@ export default function Navbar() {
           id="toggle-navbar"
           onClick={() => setNavbarIsOpen((prevCondition) => !prevCondition)}
         >
-          <HiOutlineMenuAlt3 className="h-6 w-6 text-white" />
+          <HiOutlineMenuAlt3 className="h-auto w-6 text-white md:hidden lg:w-8" />
         </button>
         <AnimatePresence>
           {navbarIsOpen && (
             <motion.ul
-              className="absolute top-0 right-0 left-0 flex h-screen flex-col items-center justify-center gap-12 bg-green-gp-800/80 backdrop-blur-sm"
+              className="absolute top-0 right-0 left-0 flex h-screen flex-col items-center justify-center gap-12 bg-green-gp-800/80 backdrop-blur-sm md:static md:h-auto md:w-1/2 md:flex-row md:justify-end md:gap-0 md:bg-transparent md:backdrop-blur-none"
               variants={ulVariants}
               initial="hidden"
               animate="visible"
               exit="exit"
             >
-              <li className="group text-2xl font-bold text-white hover:text-white/80">
+              <li className="group text-2xl font-bold text-white hover:text-white/80 md:mr-20 md:text-lg">
                 <Link to="/" className="text-current">
-                  Article
+                  Artikel
                   <hr className="h-[1px] w-1/2 border-0 bg-current duration-200 group-hover:w-full" />
                 </Link>
               </li>
               <li>
                 <SearchBar />
               </li>
-              <HiOutlineX
-                className="absolute bottom-[15%] h-12 w-12 text-white"
-                id="close-navbar"
-                onClick={() => setNavbarIsOpen(false)}
-              />
+              <li className="relative top-20 md:hidden">
+                <button type="button">
+                  <HiOutlineX
+                    className="h-12 w-12 text-white"
+                    id="close-navbar"
+                    onClick={() => setNavbarIsOpen(false)}
+                  />
+                </button>
+              </li>
             </motion.ul>
           )}
         </AnimatePresence>
@@ -91,7 +113,7 @@ function SearchBar() {
         <input
           type="search"
           id="search-bar"
-          className="form-input border-white bg-transparent text-white placeholder:text-sm placeholder:text-white/80  focus:border-current focus:ring-white/60"
+          className="form-input border-white bg-transparent text-white placeholder:text-sm placeholder:text-white/60 focus:border-current focus:ring-white/60 md:border-none md:placeholder:text-base md:focus:ring-0"
           placeholder="Telusuri ..."
         />
       </label>
