af1f69c60dd22eece37637a5601ac64f8e5fa96a
ersankarimi <ersankarimii311@gmail.com>
2022-11-16 00:57:26 +0800

    feat: wrapped the app with the `Provider` from `redux`
    Import the Provider and store, then wrapped the app with the Provider and the store value based on the store created before.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
+import store from "./app/store";
 import router from "./router";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
