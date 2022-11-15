f6e454c71b485cd132ba9a4950b367b6fc968e39
ersankarimi <ersankarimii311@gmail.com>
2022-11-16 01:16:03 +0800

    refactor: allow the app fetch the posts data
    When the app is rendered as well. The app will fetch the post data as async using the getPostsData async thunk function through the API URL stored in the .env file.
    
    The getPostsData will accept the URL parameter and the value is based on the API URL value from the .env file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 
+import { getPostsData } from "./features/posts/postsSlice";
 import { Root } from "./routes";
 
 function App() {
+  const dispatch = useDispatch();
+  const URL = import.meta.env.VITE_POSTS_API_1;
+
+  useEffect(() => {
+    dispatch(getPostsData(URL));
+  }, []);
+
   return (
     <Root>
       <Outlet />
