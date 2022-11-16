1961978000805621f2d49ce647898d6b154eec6f
ersankarimi <ersankarimii311@gmail.com>
2022-11-16 23:40:04 +0800

    refactor: update the `Articles` path file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
+import Articles from "./articles/Articles";
 import Homepage from "./homepage/Homepage";
 import Article from "./article";
-import Articles from "./articles";
 import Root from "./Root";
 
 export { Article, Articles, Homepage, Root };
