eb10a1b2c51518f623f2c3742f6dae2118018e71
ersankarimi <ersankarimii311@gmail.com>
2022-11-16 16:29:58 +0800

    refactor: remove the `useSearchParams` initialized

--- src/routes/articles.jsx
+++ src/routes/articles.jsx
@@ -1,7 +1,3 @@
-import { useSearchParams } from "react-router-dom";
-
 export default function Articles() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.get("category"));
   return <h1>Articles</h1>;
 }
