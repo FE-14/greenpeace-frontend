d38cecff191856c77a58de0f5bb7d5c40cf054f8
ersankarimi <ersankarimii311@gmail.com>
2022-11-18 23:03:00 +0800

    feat: create the read more link
    This component when clicked will redirect the user to the /articles/:articleId to read more the selected article.

---
+++ src/components/ReadMoreLink.jsx
@@ -0,0 +1,14 @@
+import { HiOutlineArrowRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
+export default function ReadMoreLink({ id }) {
+  return (
+    <Link
+      to={`/articles/${id}`}
+      relative="path"
+      className="flex items-center gap-1 font-montserrat text-xs font-bold text-submarine-gp-500 duration-150 hover:gap-2 hover:brightness-[80%] md:text-sm"
+    >
+      Baca Selengkapnya <HiOutlineArrowRight className="h-4 w-4" />
+    </Link>
+  );
+}
