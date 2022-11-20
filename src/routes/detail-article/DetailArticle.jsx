b6f3f5153cab1d9188cef8fdcce8ae327be6b132
meilyanaanisa <meilyanaanisa@gmail.com>
2022-11-20 17:00:02 +0700

    menampilkan data ke DetailArticle

--- src/routes/detail-article/DetailArticle.jsx
+++ src/routes/detail-article/DetailArticle.jsx
@@ -15,7 +15,25 @@ export default function DetailArticle() {
 
   return (
     <section className="mx-auto mt-20 md:w-10/12 lg:w-1/2">
-      <article>Data disini</article>
+      <article>
+      <div id="detail-wrapper" class="my-6">
+  <h1
+    id="blog-title"
+    class="mb-6 font-montserrat text-2xl font-bold text-black-gp-100 lg:text-4xl"
+  >{postData.title}</h1>
+  <div id="blog-detail" class="font-source-sans-pro">
+    <p id="blog-author" class="text-sm font-bold text-black-gp-100">{postData.authorName}</p>
+    <p id="blog-postdate" class="text-black-gp-secondary-black-100">{postData.postDate}</p>
+  </div>
+</div>
+
+<img id="blog-img" class="my-6 w-full object-cover" src={postData.imageUrl} />
+<p
+  id="blog-content"
+  class="my-6 flex flex-col gap-4 font-source-sans-pro text-lg"
+>{postData.postContent}</p>
+
+      </article>
     </section>
   );
 }
