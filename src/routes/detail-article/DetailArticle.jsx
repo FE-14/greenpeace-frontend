cebb9e47702cd222f722670b18df879ce04ef0e9
ersankarimi <ersankarimii311@gmail.com>
2022-11-20 21:40:13 +0800

    refactor: changes the element when `isLoading` is true
    Change the `<h1>Loading ...</h1>` element with the DetailArticleSkeleton component

--- src/routes/detail-article/DetailArticle.jsx
+++ src/routes/detail-article/DetailArticle.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 import { getDetailPostData } from "../../libs";
 
-import { Article } from "./components";
+import { Article, DetailArticleSkeleton } from "./components";
 
 export default function DetailArticle() {
   const URL = import.meta.env.VITE_POSTS_API_1;
@@ -13,7 +13,7 @@ export default function DetailArticle() {
 
   return (
     <section className="basic-padding mx-auto mt-20 py-8 md:w-10/12 lg:w-2/3">
-      {isLoading && <h1>Loading ...</h1>}
+      {isLoading && <DetailArticleSkeleton />}
       {!isLoading && Object.entries(postData).length && (
         <Article {...postData} />
       )}
