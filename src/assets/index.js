95628c02fee277108af9675bfbfa4b887902bce5
ersankarimi <ersankarimii311@gmail.com>
2022-11-23 15:09:54 +0800

    refactor: update some contents

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,23 @@
 import articleHero from "./images/article-hero.webp";
+import bgIntroAbout1 from "./images/bg-intro-about-1.png";
+import bgIntroAbout2 from "./images/bg-intro-about-2.png";
 import greenBigOutlineHalfOutboard from "./images/green-big-outline-half-artboard.png";
 import greenFullArtboard from "./images/green-full-artboard.png";
 import greenLineIllustration from "./images/green-line-illustration.png";
 import greenOutlineHalfArtboard from "./images/green-outline-half-artboard.png";
 import greenpeaceLogoWhite from "./images/greenpeace-logo-white.png";
 import homepageHero from "./images/homepage-hero.webp";
-import imageIntro from "./images/introImg1.png";
-import imageIntroCover from "./images/introImg2.png";
 import whiteLineIllustration from "./images/white-line-illustration.png";
 
 export {
   articleHero,
+  bgIntroAbout1,
+  bgIntroAbout2,
   greenBigOutlineHalfOutboard,
   greenFullArtboard,
   greenLineIllustration,
   greenOutlineHalfArtboard,
   greenpeaceLogoWhite,
   homepageHero,
-  imageIntro,
-  imageIntroCover,
   whiteLineIllustration,
 };
