8df7e9a8dd44f1f66a429aff51ab2bea81990e73
ersankarimi <ersankarimii311@gmail.com>
2022-11-23 22:32:48 +0800

    refactor: import and re-export `History` route page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import Articles from "./articles/Articles";
 import DetailArticle from "./detail-article/DetailArticle";
+import History from "./history/History";
 import Homepage from "./homepage/Homepage";
 import Error from "./Error";
 import Root from "./Root";
 import Search from "./Search";
 
-export { Articles, DetailArticle, Error, Homepage, Root, Search };
+export { Articles, DetailArticle, Error, History, Homepage, Root, Search };
