e4d14b1210abb1cffba76d65794b143286e818a9
ersankarimi <ersankarimii311@gmail.com>
2022-11-24 21:33:27 +0800

    feat: import and re-export `About` page route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,18 @@ import Articles from "./articles/Articles";
 import DetailArticle from "./detail-article/DetailArticle";
 import History from "./history/History";
 import Homepage from "./homepage/Homepage";
+import About from "./About";
 import Error from "./Error";
 import Root from "./Root";
 import Search from "./Search";
 
-export { Articles, DetailArticle, Error, History, Homepage, Root, Search };
+export {
+  About,
+  Articles,
+  DetailArticle,
+  Error,
+  History,
+  Homepage,
+  Root,
+  Search,
+};
