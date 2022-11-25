d9cf01da9a27ae43e9ff1c494aabbf94ba78400f
ersankarimi <ersankarimii311@gmail.com>
2022-11-25 21:16:50 +0800

    refactor: update some contents and change the images path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,8 @@ import { Link, ScrollRestoration } from "react-router-dom";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from "prop-types";
 
+import { greenpeaceLogoWhite, whiteLineIllustration } from "../assets";
+
 export default function Footer() {
   const contacts = [
     {
@@ -32,80 +34,106 @@ export default function Footer() {
     },
   ];
 
-  const articlesSubSections = [
-    "Tentang Kami",
-    "Sejarah Greenpeace",
-    "Kemenangan",
-  ];
-  const articlesSubSectionstwo = ["Semua", "Krisis Iklim", "Hutan", "Polusi"];
-
   return (
-    <footer className="flex w-full flex-col items-center bg-green-gp-900 py-4">
+    <footer className="flex w-full flex-col items-center bg-green-gp-900 py-8">
       <ScrollRestoration />
-      <div className="container flex items-center justify-between p-4">
-        <div className="mb-8 flex flex-col items-center justify-center gap-4 text-left">
-          <h2 className="border-b-2 border-b-green-gp-400 font-montserrat text-xl font-bold text-green-400 md:text-xl">
-            Kenali Greenpeace
-          </h2>
-          <ul className="flex flex-col text-white pr-14 md:gap-1 lg:gap-2">
-            {articlesSubSections.map((article) => (
-              <li key={article}>
-                <FooterLink
-                  to={`${article}`}
-                  className="hover:brightness-[80%]"
-                >
-                  {article}
+      <div className="flex w-full flex-col items-center justify-between gap-12 p-12 lg:flex-row">
+        <section className="flex flex-col items-start gap-8 lg:flex-row">
+          <div className="flex flex-col items-center justify-center gap-4 p-4 lg:items-start">
+            <h2 className="border-b-2 border-b-green-gp-400 font-montserrat text-xl font-bold text-green-400 md:text-2xl">
+              Kenali Greenpeace
+            </h2>
+            <ul className="flex items-center gap-4 text-center text-white md:gap-8 lg:flex-col lg:items-start lg:gap-2">
+              <li>
+                <FooterLink to="/about" className="hover:brightness-[80%]">
+                  Tentang Kami
                 </FooterLink>
               </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="mb-8 flex flex-col items-center gap-4 text-left">
-          <h2 className="border-b-2 border-b-green-gp-400 font-montserrat text-xl font-bold text-green-400 md:text-xl">
-            Artikel
-          </h2>
-          <ul className="flex flex-col gap-4 text-white md:gap-1 lg:gap-2">
-            {articlesSubSectionstwo.map((section) => (
-              <li key={section}>
-                <FooterLink
-                  to={`articles?category=${section}`}
-                  className="hover:brightness-[80%]"
-                >
-                  {section}
+              <li>
+                <FooterLink to="/history" className="hover:brightness-[80%]">
+                  Sejarah
                 </FooterLink>
               </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="mt-12 flex flex-col justify-center gap-2 text-center font-montserrat text-white md:gap-2 xl:gap-4">
-          <h2 className="text-xl font-semibold md:text-2xl">Contact Us</h2>
-          <div className="flex items-center gap-4 text-xl xl:text-2xl">
-            {contacts.map((contact) => (
-              <FooterLink
-                key={contact.title}
-                to={contact.url}
-                type="original"
-                target="_blank"
-              >
-                {contact.icon}
-              </FooterLink>
-            ))}
+              <li>
+                <FooterLink
+                  to="/our-victory"
+                  className="hover:brightness-[80%]"
+                >
+                  Kemenangan
+                </FooterLink>
+              </li>
+            </ul>
           </div>
-        </div>
+          <div className="flex flex-col items-center justify-center gap-4 p-4">
+            <h2 className="border-b-2 border-b-green-gp-400 font-montserrat text-xl font-bold text-green-400 md:text-2xl">
+              Artikel
+            </h2>
+            <ul className="flex items-center gap-4 text-center text-white md:gap-8 lg:flex-col lg:items-start lg:gap-2">
+              <li>
+                <FooterLink
+                  to="articles?category=Semua"
+                  className="hover:brightness-[80%]"
+                >
+                  Semua
+                </FooterLink>
+              </li>
+              <li>
+                <FooterLink
+                  to="articles?category=Krisis Iklim"
+                  className="hover:brightness-[80%]"
+                >
+                  Krisis Iklim
+                </FooterLink>
+              </li>
+              <li>
+                <FooterLink
+                  to="articles?category=Hutan"
+                  className="hover:brightness-[80%]"
+                >
+                  Hutan
+                </FooterLink>
+              </li>
+              <li>
+                <FooterLink
+                  to="articles?category=Polusi"
+                  className="hover:brightness-[80%]"
+                >
+                  Polusi
+                </FooterLink>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section>
+          <div className="mb-12 flex flex-col justify-center gap-2 text-center font-montserrat text-white md:gap-3 xl:gap-4">
+            <h2 className="text-xl font-semibold md:text-2xl">Contact Us</h2>
+            <div className="flex items-center gap-4 text-xl xl:text-2xl">
+              {contacts.map((contact) => (
+                <FooterLink
+                  key={contact.title}
+                  to={contact.url}
+                  type="original"
+                  target="_blank"
+                >
+                  {contact.icon}
+                </FooterLink>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
 
       <div className="flex w-full items-center">
         <img
-          src="/src/assets/images/greenpeace-logo-white.png"
+          src={greenpeaceLogoWhite}
           alt="Greenpeace Logo"
-          className="absolute left-8 h-8 bg-green-gp-900 px-4 md:left-16 md:h-6 lg:px-12"
+          className="absolute left-8 h-8 bg-green-gp-900 px-4 md:left-16 md:h-11 lg:px-12"
         />
         <img
-          src="/src/assets/images/white-line-illustration.png"
+          src={whiteLineIllustration}
           alt="Artboard Illustration"
-          className="h-8 w-full bg-cover bg-repeat-x object-cover md:h-6"
+          className="h-8 w-full bg-cover bg-repeat-x object-cover md:h-10"
         />
       </div>
     </footer>
