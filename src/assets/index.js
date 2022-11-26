30c599be82981c9783a1203e8bacc448dbde6028
ersankarimi <ersankarimii311@gmail.com>
2022-11-26 22:33:58 +0800

    refactor: changes some images format

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,16 +1,17 @@
 import aboutUsLogo from "./images/about-logo.png";
-import artboardRectangle from "./images/artboard-rectangle.png";
+import artboardRectangle from "./images/artboard-rectangle.webp";
 import articleHero from "./images/article-hero.webp";
-import bgIntroAbout1 from "./images/bg-intro-about-1.png";
-import bgIntroAbout2 from "./images/bg-intro-about-2.png";
-import greenBigOutlineHalfOutboard from "./images/green-big-outline-half-artboard.png";
-import greenFullArtboard from "./images/green-full-artboard.png";
-import greenLineIllustration from "./images/green-line-illustration.png";
-import greenOutlineHalfArtboard from "./images/green-outline-half-artboard.png";
+import bgIntroAbout1 from "./images/bg-intro-about-1.webp";
+import bgIntroAbout2 from "./images/bg-intro-about-2.webp";
+import greenBigOutlineHalfOutboard from "./images/green-big-outline-half-artboard.webp";
+import greenFullArtboard from "./images/green-full-artboard.webp";
+import greenLineIllustration from "./images/green-line-illustration.webp";
+import greenOutlineHalfArtboard from "./images/green-outline-half-artboard.webp";
 import greenpeaceLogoWhite from "./images/greenpeace-logo-white.png";
-import historyCover from "./images/history-cover.png";
+import historyCover from "./images/history-cover.webp";
 import historyLogo from "./images/history-logo.png";
 import homepageHero from "./images/homepage-hero.webp";
+import ourVictoryCover from "./images/our-victory-cover.webp";
 import victoryLogo from "./images/victory-logo.png";
 import whiteLineIllustration from "./images/white-line-illustration.png";
 
@@ -28,6 +29,7 @@ export {
   historyCover,
   historyLogo,
   homepageHero,
+  ourVictoryCover,
   victoryLogo,
   whiteLineIllustration,
 };
