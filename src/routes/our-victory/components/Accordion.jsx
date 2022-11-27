310f2e12eee6e4e22531ea2df9f1c6a0a0819eca
ersankarimi <ersankarimii311@gmail.com>
2022-11-27 14:48:17 +0800

    refactor: add `data` props
    The data prop is contains the accordion data that will be iterate and each data will return the AccordionItem component

--- src/routes/our-victory/components/Accordion.jsx
+++ src/routes/our-victory/components/Accordion.jsx
@@ -1,3 +1,11 @@
-export default function Accordion() {
-  return <h1>asdasdasdas</h1>;
+import AccordionItem from "./AccordionItem";
+
+export default function Accordion({ data }) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-8">
+      {data.map((item) => (
+        <AccordionItem {...item} />
+      ))}
+    </div>
+  );
 }
