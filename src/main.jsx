911496bcc6710362da5d518d96c5683f0709b12c
ersankarimi <ersankarimii311@gmail.com>
2022-12-01 14:55:08 +0800

    refactor: add the `AuthProvider` to wrapped whole app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
 import store from "./app/store";
+import { AuthProvider } from "./context/AuthProvider";
 import router from "./router";
 
 import "react-loading-skeleton/dist/skeleton.css";
@@ -11,8 +12,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
